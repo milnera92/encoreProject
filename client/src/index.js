c88ec07b970c221916f9fa127fd405af396fc7f1
Andrew <milnera92@outlook.com>
2023-01-12 13:08:34 -0500

    ready to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
 import App from "./App";
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 // make env for auth0
 // alt is window.__RUNTIME_CONFIG__ instead of process.env
